54455c07e41c8c540e90a995f18af1252cecd0bb
A1phaGo <bettlm@bupt.edu.cn>
2017-07-13 16:16:24 +0800

    进行Spring框架测试的maven项目
    基于《轻量级Java EE企业应用实战》这本书

---
+++ mySpring/src/main/java/com/lm/mySpring/App.java
@@ -0,0 +1,13 @@
+package com.lm.mySpring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ mySpring/src/main/java/com/lm/mySpring/Axe.java
@@ -0,0 +1,7 @@
+package com.lm.mySpring;
+
+public class Axe {
+	public void chop() {
+		System.out.println("砍柴");
+	}
+}
