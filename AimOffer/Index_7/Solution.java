18a1c3a64659e84377b13eebe96e9f1159baf1b1
tlm-bupt <bettlm@bupt.edu.cn>
2017-10-23 16:58:09 +0800

    AimOffer

---
+++ AimOffer/Index_7/Solution.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package Index_7;
+
+import java.util.Arrays;
+
+/**
+ * @author tlm
+ * @aim offer : 斐波那契数列
+ * @题目描述 : 大家都知道斐波那契数列，现在要求输入一个整数n，请你输出斐波那契数列的第n项。 n<=39
+ */
+
+public class Solution {
+
+	public int Fibonacci(int n) {
+		if (n == 0) {
+			return 0;
+		}
+		if (n<=2) {
+			return 1;
+		}
+		int result[] = new int[n + 1];
+		result[1] = 1;
+		result[2] = 1;
+		for (int i = 3; i <= n; i++) {
+			result[i] = result[i - 1] + result[i - 2];
+		}
+		System.out.println(Arrays.toString(result));
+		return result[n];
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		System.out.println(solution.Fibonacci(3));
+	}
+
+}
---
+++ AimOffer/Index_9/Solution.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package Index_9;
+
+/**
+ * @author tlm
+ * @aim offer : 变态跳台阶
+ * @题目描述 : 一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
+ */
+
+public class Solution {
+	public int JumpFloor(int target) {
+		if (target == 0) {
+			return 0;
+		}
+		if (target == 1) {
+			return 1;
+		}
+		if (target == 2) {
+			return 2;
+		}
+		int result[] = new int[target + 1];
+		result[0] = 1;
+		result[1] = 1;
+		result[2] = 2;
+		for (int i = 3; i <= target; i++) {
+			result[i] = 0;
+			for (int j = 0; j < i; j++) {
+				result[i] += result[j];
+			}
+		}
+		return result[target];
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		System.out.println();
+	}
+
+}
