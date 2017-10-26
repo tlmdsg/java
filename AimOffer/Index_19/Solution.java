6971635387c87879fe80333da71835687933a0c4
tlm-bupt <bettlm@bupt.edu.cn>
2017-10-26 09:43:04 +0800

    AimOffer

---
+++ AimOffer/Index_19/Solution.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package Index_19;
+
+import java.util.LinkedList;
+
+/**
+ * @author tlm
+ * @aim offer : 包含min函数的栈
+ * @题目描述 : 定义栈的数据结构，请在该类型中实现一个能够得到栈最小元素的min函数。
+ */
+
+public class Solution {
+
+	LinkedList<Integer> stack = new LinkedList<>();
+
+	public void push(int node) {
+		stack.addFirst(node);
+	}
+
+	public void pop() {
+		stack.removeFirst();
+	}
+
+	public int top() {
+		return stack.peek();
+	}
+
+	public int min() {
+		if (stack.isEmpty()) {
+			return 0;
+		}
+		int m = stack.peek();
+		for (Integer integer : stack) {
+			m = integer < m ? integer : m;
+		}
+		return m;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		System.out.println();
+	}
+
+}
---
+++ AimOffer/Index_21/Solution.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package Index_21;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author tlm
+ * @aim offer : 从上往下打印二叉树
+ * @题目描述 : 从上往下打印出二叉树的每个节点，同层节点从左至右打印。
+ */
+
+class TreeNode {
+	int val = 0;
+	TreeNode left = null;
+	TreeNode right = null;
+
+	public TreeNode(int val) {
+		this.val = val;
+	}
+}
+
+public class Solution {
+
+	public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+		ArrayList<Integer> result = new ArrayList<>();
+		if (root == null) {
+			return result;
+		}
+		Queue<TreeNode> queue = new LinkedList<>();
+		queue.offer(root);
+		while (!queue.isEmpty()) {
+			TreeNode curr = queue.poll();
+			if (curr.left != null) {
+				queue.offer(curr.left);
+			}
+			if (curr.right != null) {
+				queue.offer(curr.right);
+			}
+			result.add(curr.val);
+		}
+		return result;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		System.out.println();
+	}
+
+}
